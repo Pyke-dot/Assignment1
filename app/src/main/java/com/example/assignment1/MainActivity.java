ccd1dc21479bf6a0433774cb26fc10ee555119a8
Tecca Yu <onionringsaregood@gmail.com>
2022-02-02 16:16:02 -0500

    revised onclick

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +12,16 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     public TextView display;
-
-    Button one_btn, two_btn,three_btn, four_btn, five_btn, six_btn, seven_btn, eight_btn, nine_btn, zero_btn,
-            add_btn, subtract_btn, multiply_btn, divide_btn, reset_btn, equal_btn, advance_btn, pow_btn, remainder_btn,
-            max_btn,min_btn;
-
     public static ArrayList<String> userInput = new ArrayList<>();
+    Button one_btn, two_btn,three_btn, four_btn, five_btn, six_btn, seven_btn, eight_btn, nine_btn, zero_btn,
+            add_btn, subtract_btn, multiply_btn, divide_btn, equal_btn, advance_btn, pow_btn, remainder_btn,
+            max_btn,min_btn,reset_btn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         display = findViewById(R.id.input);
 
         zero_btn = findViewById(R.id.zero);      //0
@@ -58,8 +56,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         advance_btn.setOnClickListener(this);
         equal_btn = findViewById(R.id.equal);        //equal
         equal_btn.setOnClickListener(this);
-        reset_btn = findViewById(R.id.clear_text);        //reset
-        reset_btn.setOnClickListener(this);
         pow_btn = findViewById(R.id.power);        //pow
         pow_btn.setOnClickListener(this);
         remainder_btn = findViewById(R.id.remainder);        //remainder
@@ -68,6 +64,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         max_btn.setOnClickListener(this);
         min_btn = findViewById(R.id.min);        //min
         min_btn.setOnClickListener(this);
+        reset_btn = findViewById(R.id.clear_text);        //reset
+        reset_btn.setOnClickListener(this);
     }
 
     public void push(String strToAdd){
@@ -76,150 +74,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         display.setText(String.format("%s %s",oldStr,strToAdd));
     }
 
-
-    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
-        int id = view.getId();
-        switch(id) {
-            case R.id.one :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.one).toString());
-                }else {
-                    push(getText(R.string.one).toString());
-                }
-                break;
-            case R.id.two:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.two).toString());
-                }else {
-                    push(getText(R.string.two).toString());
-                }
-                break;
-            case R.id.three :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.three).toString());
-                }else {
-                    push(getText(R.string.three).toString());
-                }
-                break;
-            case R.id.four:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.four).toString());
-                }else {
-                    push(getText(R.string.four).toString());
-                }
-                break;
-            case R.id.five :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.five).toString());
-                }else {
-                    push(getText(R.string.five).toString());
-                }
-                break;
-            case R.id.six:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.six).toString());
-                }else {
-                    push(getText(R.string.six).toString());
-                }
-                break;
-            case R.id.seven :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.seven).toString());
-                }else {
-                    push(getText(R.string.seven).toString());
-                }
-                break;
-            case R.id.eight:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.eight).toString());
-                }else {
-                    push(getText(R.string.eight).toString());
-                }
-                break;
-            case R.id.nine :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.nine).toString());
-                }else {
-                    push(getText(R.string.nine).toString());
-                }
-                break;
-            case R.id.zero:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.zero).toString());
-                }else {
-                    push(getText(R.string.zero).toString());
-                }
-                break;
-            case R.id.add :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.add).toString());
-                }else {
-                    push(getText(R.string.add).toString());
-                }
-                break;
-            case R.id.multiply:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.multiply).toString());
-                }else {
-                    push(getText(R.string.multiply).toString());
-                }
-                break;
-            case R.id.subtract :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.subtract).toString());
-                }else {
-                    push(getText(R.string.subtract).toString());
-                }
-                break;
-            case R.id.divide:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.divide).toString());
-                }else {
-                    push(getText(R.string.divide).toString());
-                }
-                break;
-            case R.id.clear_text:
+        String title = ((Button)view).getText().toString();
+        switch(title) {
+            case "C":
                 display.setText("");
                 userInput.clear();
-                break;
-            case R.id.equal:
+            case "=":
                 String userExpression = display.getText().toString();
-                if(userExpression.isEmpty()){
+                if(userExpression.isEmpty() || display.getText().toString().contains("=")){
                     display.setText("");
-                    userInput.clear();
-                }else if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
                 }
                 else{
                     String result = String.valueOf(Calculator.calculate());
@@ -228,10 +93,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     } else {
                         push("= " + result);
                     }
-                    userInput.clear();
                 }
+                userInput.clear();
                 break;
-            case R.id.advance :
+            case "advance" :
                 TextView advance = findViewById(R.id.advance);
                 if(getText(R.string.advanceText).toString().equals(advance.getText().toString())) {
                     advance.setText(R.string.standardText);
@@ -241,43 +106,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     findViewById(R.id.scientific).setVisibility(View.GONE);
                 }
                 break;
-            case R.id.power:
+            default:
                 if(display.getText().toString().contains("=")){
                     display.setText("");
                     userInput.clear();
-                    push(getText(R.string.power).toString());
-                }else {
-                    push(getText(R.string.power).toString());
                 }
+                push(title);
                 break;
-            case R.id.remainder :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.remainder).toString());
-                }else {
-                    push(getText(R.string.remainder).toString());
-                }
-                break;
-            case R.id.max:
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.max).toString());
-                }else {
-                    push(getText(R.string.max).toString());
-                }
-                break;
-            case R.id.min :
-                if(display.getText().toString().contains("=")){
-                    display.setText("");
-                    userInput.clear();
-                    push(getText(R.string.min).toString());
-                }else {
-                    push(getText(R.string.min).toString());
-                }
-                break;
-
         }
     }
+
+
 }
